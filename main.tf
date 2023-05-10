5b5a8b9a93e5017d88c2b10a42034d46d17f576b
Anudeep Reddy Vanga <vareddy101091@gmail.com>
2023-05-10 18:06:38 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -5,3 +5,13 @@ resource "aws_ssm_parameter" "parameters" {
   type   = "String"
   key_id = "66ecc05d-1549-4ecf-8959-2cbcf2bec975"
 }
+
+## Passwords are not at all meant to keep inside the code at all, Here since it is a lab we are doing it to save the time.
+## One of the scenario that mostly used in real way is , Enter the passwords manually in Parameter Store by someone for passwords.
+resource "aws_ssm_parameter" "passwords" {
+  count  = length(var.passwords)
+  name   = var.passwords[count.index].name
+  value  = var.passwords[count.index].value
+  type   = "SecureString"
+  key_id = "66ecc05d-1549-4ecf-8959-2cbcf2bec975"
+}
--- vars.tf
+++ vars.tf
@@ -1,1 +1,2 @@
 variable "parameters" {}
+variable "passwords" {}
